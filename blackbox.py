3179a5dc523c8b8f2dcfc289dec56148ef772da7
Michel <michael.milawski@planet-itservices.com>
2020-05-31 18:23:54 +0200

    auto screenshots

--- blackbox.py
+++ blackbox.py
@@ -2,8 +2,11 @@ import threading
 import time
 import activewindow
 import app
+import time
+import logging
 
 from screenshot import Screenshot
+
 
 class Blackbox(object):
     """ Threading example class
@@ -18,11 +21,12 @@ class Blackbox(object):
         """
         self.interval = interval
         self.active_window = {}
+        self.last_screenshot = {}
+        self.screenshot_saved_at = 0
 
         thread = threading.Thread(target=self.run, args=())
         thread.daemon = True  # Daemonize thread
         thread.start()  # Start the execution
-
 
     def run(self):
         """ Method that runs forever """
@@ -30,26 +34,39 @@ class Blackbox(object):
 
             try:
                 current_window = activewindow.get_active_window3()
+
+                s = Screenshot(current_window)
+                s.takeScreenshot()
+
+                "compare the last and the current screenshot"
+                screenshot_changed = self.last_screenshot != s.image
+
                 if current_window.title != self.active_window:
                     # New Window detected
+                    print("NEW window detected")
+                    s.save()
+                    self.screenshot_saved_at = time.time()
                     self.active_window = current_window.title
 
-                    s = Screenshot(current_window)
-                    s.makeCoordinatedScreenshot()
+
+                else:
+                    seconds_since_last_screenshot = time.time() - self.screenshot_saved_at
+                    if screenshot_changed and seconds_since_last_screenshot > 10:
+                        print("Same window and a screenshot changed after a while")
+                        s.save()
+                        self.screenshot_saved_at = time.time()
+
+                self.last_screenshot = s.image
 
             except Exception as err:
                 print("{0}".format(err))
                 print("Some error occured!")
 
-
             time.sleep(self.interval)
 
+logging.disable(logging.CRITICAL)
 
-
-example = Blackbox()
-print('Checkpoint')
-
+Blackbox()
 
 while True:
     time.sleep(2)
-
--- screenshot.py
+++ screenshot.py
@@ -9,34 +9,38 @@ from multiprocessing import Process, Queue
 from subprocess import Popen, PIPE
 import re
 
+
 class Screenshot:
     screenshotPath = 'screenshots'
 
     def __init__(self, log_entry: app.LogEntry):
         self.log_entry = log_entry
+        self.image = None
         Path(self.screenshotPath).mkdir(parents=True, exist_ok=True)
+
 
     def getFilename(self):
         now = datetime.now()
         currentDayPath = self.screenshotPath + "/" + now.strftime("%Y/%m/%Y-%m-%d/")
         Path(currentDayPath).mkdir(parents=True, exist_ok=True)
-        fullPath = currentDayPath + now.strftime("%Y-%m-%d--%H-%M-%S") + "-{mon}-{top}-{left}-{width}-{height}-{date}.jpg"
+        fullPath = currentDayPath + now.strftime("%Y-%m-%d--%H-%M-%S") + ".jpg"
         return fullPath
 
-    "create a screenshot of all desktops"
     def makeGlobalScreenshot(self):
+        """create a screenshot of all desktops"""
+
         with mss.mss() as sct:
             filename = self.getFilename()
             print("Creating screenshot " + filename)
             sct.shot(mon=-1, output=filename)
 
-    def makeCoordinatedScreenshot(self):
-        le = self.log_entry
+    def save(self):
+        """save the taken screenshot to disk"""
+
         filename = self.getFilename()
-        
-        window=(le.window_location.x,le.window_location.y,le.window_size.width,le.window_size.height)
+        self.image.save(filename)
 
-        image = pyautogui.screenshot( region=window  )
-        image.save(filename)
-        #image = pyautogui.screenshot( region=(le.window_location.x,le.window_location.y,le.window_size.width,le.window_size.height)  )
-
+    def takeScreenshot(self):
+        le = self.log_entry
+        window = (le.window_location.x, le.window_location.y, le.window_size.width, le.window_size.height)
+        self.image = pyautogui.screenshot(region=window)
